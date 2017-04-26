a3a948f27c389c30516c4ff43528d627913960ff
Narendran Srinivasan <nare469@gmail.com>
2017-04-26 12:40:50 -0400

    Fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 
 set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $HOME/bin $GOPATH/bin
 
 # Enable vi mode
 set fish_key_bindings fish_vi_key_bindings
@@ -9,3 +9,4 @@ set fish_key_bindings fish_vi_key_bindings
 set -g theme_display_vi yes
 set -g theme_date_format "+"
 set -g theme_show_exit_status no
+set -g theme_color_scheme gruvbox
--- fish/.config/fish/functions/r101apish.fish
+++
@@ -1,3 +0,0 @@
-function r101apish
-  /Users/narendransrinivasan/remind/r101-development/bin/dc-local run api-web1 bash
-end
