2d7c11a4588637540f6cd41ae67cf11a4c4043d5
Narendran Srinivasan <nare469@gmail.com>
2017-04-28 16:21:07 -0400

    Config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
 
 set -x GOPATH $HOME/go
-set -x PATH $PATH $HOME/bin $GOPATH/bin
+set -x PATH $PATH $HOME/bin $GOPATH/bin $HOME/.gem/ruby/2.4.0/bin
+
+# Reload colourscheme
+wal -r
 
 # Enable vi mode
 set fish_key_bindings fish_vi_key_bindings
